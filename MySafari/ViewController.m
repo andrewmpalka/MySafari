b1e1c5817dfbd0d6757271df788d0c106f9b252c
mouerj <joseph.mouer@icloud.com>
2016-01-12 18:21:53 -0600

    completed step 11 not step 10

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -24,6 +24,16 @@
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)urlTextField {
+    if ([urlTextField.text containsString:@"http://wwww."] == YES)  {
+        urlTextField.text = urlTextField.text;
+    } else if ([urlTextField.text containsString:@"https://wwww."] == YES) {
+        urlTextField.text = urlTextField.text;
+    } else if ([urlTextField.text containsString:@"wwww."] == YES) {
+        urlTextField.text = [NSString stringWithFormat:@"http://%@", urlTextField.text];
+    } else {
+        urlTextField.text = [NSString stringWithFormat:@"http://www.%@", urlTextField.text];
+    }
+    
     NSURL *url = [NSURL URLWithString:urlTextField.text];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
@@ -36,10 +46,20 @@
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView {
     [self.spinner stopAnimating];
-    [self.spinner hidesWhenStopped];
+    self.spinner.hidesWhenStopped = YES;
 }
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
     [self.webView goBack];
+}
+- (IBAction)onForwardButtonPressed:(UIButton *)sender {
+    [self.webView goForward];
+}
+- (IBAction)onStopLoadingButtonPressed:(UIButton *)sender {
+    [self.webView stopLoading];
+    
+}
+- (IBAction)onReloadButtonPressed:(UIButton *)sender {
+    [self.webView reload];
 }
 
 @end
